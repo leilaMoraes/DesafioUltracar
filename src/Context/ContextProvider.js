b83ae2549fcaf8398e84922cac36b0311b64f815
Leila Moraes <leila.violino@gmail.com>
2023-04-17 12:47:03 -0300

    feat: configuração do Context

---
+++ src/Context/ContextProvider.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import ContextApp from './ContextApp';
+
+export default function ContextProvider({ children }) {
+
+  return (
+    <ContextApp.Provider>
+      {children}
+    </ContextApp.Provider>
+  );
+}
+
+ContextProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
