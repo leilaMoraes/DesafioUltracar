32f927e96136cc25e63cd7e18a62395b016abe49
Leila Moraes <leila.violino@gmail.com>
2023-04-17 12:34:03 -0300

    feat: configurações iniciais

---
+++ src/Pages/InitialPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function InitialPage() {
+  return (
+    <header>Hello World</header>
+  );
+}
+
+export default InitialPage;
---
+++ src/Routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import InitialPage from './Pages/InitialPage';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/" component={ InitialPage } />
+    </Switch>
+  );
+}
