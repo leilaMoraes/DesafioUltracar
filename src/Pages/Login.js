a26b1c4cf8a9ff03c520ed0a7d28e13daa41bf4d
Leila Moraes <leila.violino@gmail.com>
2023-04-18 14:55:33 -0300

    chore: css da página de login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Button, Select } from 'antd';
+import { Button, Select, Space } from 'antd';
 import Password from 'antd/es/input/Password';
 import AppContext from '../Context/AppContext';
 
@@ -12,29 +12,43 @@ function Login() {
   };
 
   return (
-    <div>
-      <Select
-        value={ selectedEmployee }
-        onChange={ onChange }
-      >
-        { selectEmployee.map((e, i) => (
-          <Select.Option key={ i }>{ e.name }</Select.Option>
-        ))}
-      </Select>
-      <Password
-        type="password"
-        name="password"
-        placeholder="Digite sua senha"
-        onChange={ onChange }
-      />
-      <Button
-        type="primary"
-        disabled={ compare }
-        onClick={ onClick }
-      >
-        Entrar
-      </Button>
-    </div>
+    <Space direction="vertical">
+      <div>
+        <img src="https://ultracar.com.br/sistema-gestao-oficina-mecanica/programa-gerenciamento-oficina-mecanica/wp-content/uploads/2019/09/LOGO-TOPO-SITE.png" alt="Ultracar - Sistema de gestão completo para oficinas automotivas" />
+      </div>
+      <Space direction="vertical">
+        <Select
+          style={ { width: '20em' } }
+          value={ selectedEmployee }
+          onChange={ onChange }
+        >
+          { selectEmployee.map((e, i) => (
+            <Select.Option
+              key={ i }
+            >
+              { e.name }
+            </Select.Option>
+          ))}
+        </Select>
+        <Password
+          style={ { width: '20em' } }
+          type="password"
+          name="password"
+          placeholder="Digite sua senha"
+          onChange={ onChange }
+        />
+        <Button
+          style={ { width: '20em',
+            backgroundColor: 'rgb(123, 220, 181)',
+            fontWeight: 'bold' } }
+          type="primary"
+          disabled={ compare }
+          onClick={ onClick }
+        >
+          Entrar
+        </Button>
+      </Space>
+    </Space>
   );
 }
 
